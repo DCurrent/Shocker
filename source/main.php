9cf17156dad9526b9bae6cefd51693d056303fb2
Damon V. Caskey <dc@caskeys.com>
2017-09-06 19:52:06 -0400

    Use 'shocker_' session prefix for session SPs.

--- source/main.php
+++ source/main.php
@@ -70,6 +70,8 @@
 		// Prepare session handler configuration.
 		$session_config = new \dc\nahoni\SessionConfig();
 		$session_config->set_database($yukon_database);
+		$session_config->set_sp_prefix('shocker_');	// Set prefix for SP names.
+	
 
 		$session_handler = new \dc\nahoni\Session($session_config);
 		session_set_save_handler($session_handler, TRUE);	
