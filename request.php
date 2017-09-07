e023f460fea1610e2ee381cb3be2cade0b6d7788
Damon V. Caskey <dc@caskeys.com>
2017-09-07 08:55:19 -0400

    Fix error caused by missing variables in email code.

--- request.php
+++ request.php
@@ -128,7 +128,6 @@
 			$subject = MAILING::SUBJECT;
 			$body = 'AED Request posted. <a href="http://ehs.uky.edu/apps/shocker/request_read.php?&id='.$_main_data->get_id().'">Click here</a> to view.';
 
-			$address = $post->get_email();
 			$headers   = array();
 			$headers[] = "MIME-Version: 1.0";
 			$headers[] = "Content-type: text/html; charset=iso-8859-1";
@@ -138,8 +137,7 @@
 
 			// Set up mail to addresses.
 			$mail_to = MAILING::TO;
-			$mail_to .= ', '.$oAcc->get_account().'@uky.edu';
-			if($address && filter_var($address, FILTER_VALIDATE_EMAIL)) $mail_to .= ', '.$address;
+			//$mail_to .= ', '.$access_obj->get_account().'@uky.edu, '.$_main_data->get_account().'@uky.edu';
 					
 			// Run mail function.
 			mail($mail_to, MAILING::SUBJECT, $body, implode("\r\n", $headers));
